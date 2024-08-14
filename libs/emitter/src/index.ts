e03e1e43bfab7e6bb4dc390d682fb344507f2cd5
Mindy Lim <build@everleagues.com>
2024-08-14 18:02:23 +0800

    export withNgxsEmitPlugin

--- libs/emitter/src/index.ts
+++ libs/emitter/src/index.ts
@@ -2,7 +2,7 @@ export { Receiver } from './lib/core/decorators/receiver';
 export { Emitter } from './lib/core/decorators/emitter';
 export { Emittable, OfEmittableActionContext } from './lib/core/internal/internals';
 export { EmitterAction } from './lib/core/actions/actions';
-export { NgxsEmitPluginModule } from './lib/emit.module';
+export { NgxsEmitPluginModule, withNgxsEmitPlugin } from './lib/emit.module';
 export { EmitterService } from './lib/core/services/emitter.service';
 export {
   ofEmittableDispatched,
