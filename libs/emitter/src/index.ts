900cad77d78c285985a74690f888b0159fcc4b03
Mindy Lim <build@everleagues.com>
2024-08-20 16:09:49 +0800

    fix

--- libs/emitter/src/index.ts
+++ libs/emitter/src/index.ts
@@ -1,3 +1,4 @@
+export { EmitStore } from './lib/core/services/emit-store.service';
 export { Receiver } from './lib/core/decorators/receiver';
 export { Emitter } from './lib/core/decorators/emitter';
 export { Emittable, OfEmittableActionContext } from './lib/core/internal/internals';
